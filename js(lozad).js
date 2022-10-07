bc2175063b7cef67114c3fff98a0885a117ff85b
alex6061 <39526012+alex6061@users.noreply.github.com>
2022-10-07 21:09:33 +0300

    git

---
+++ js(lozad).js
@@ -0,0 +1,8 @@
+lozad('.lozad', {
+    load: function(el) {
+        console.log('loading element');
+
+        // Custom implementation to load an element
+        // e.g. el.src = el.getAttribute('data-src');
+    }
+});
